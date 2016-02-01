6c79b1af5a6df8ae9e14ccbcaaf22e0bb947d42a
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-01 14:48:07 +0000

    Runs the command line game from a separate repository

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Apprenticeship-RubyCommandLineTicTacToe
+
+Command Line Tic Tac Toe game implemented in Ruby
+
+To run the application clone the project into a folder
+
+>> git clone git@github.com:gemcfadyen/Apprenticeship-RubyCommandLineTicTacToe.git
+
+
+From this folder run the command
+
+>> bin/run.sh
+
+The game will then start. To make a move, enter a number via the commandline when prompted. If the move is not valid, you will be reprompted until a valid move is given.
+
+Once the game is over, the winner will be announced, or the game will be a draw.
+
+You can replay by entering Y or y when prompted.
---
+++ bin/run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+#Setup Load Path
+lib = File.expand_path("../../lib", __FILE__)
+ttt_lib = File.expand_path("../../../Apprenticeship-RubyTicTacToe/lib", __FILE__)
+$:.unshift(lib).unshift(ttt_lib)
+
+require 'command_line_app'
+require 'command_line_ui'
+require 'board_factory'
+require 'command_line_player_factory'
+require 'prompt_writer'
+require 'prompt_reader'
+
+writer = PromptWriter.new($stdout)
+reader = PromptReader.new($stdin)
+command_line_ui = CommandLineUI.new(writer, reader)
+
+board_factory = BoardFactory.new
+player_factory = CommandLinePlayerFactory.new
+
+command_line_app = CommandLineApp.new(command_line_ui, board_factory, player_factory)
+command_line_app.start
